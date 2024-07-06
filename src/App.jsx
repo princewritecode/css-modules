758aeab87be184ce44b765ad5e7595193d2abd27
princewritecode <princewritecode@gmail.com>
2024-07-06 20:06:23 +0530

    healthy food version 1.1

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,24 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 import ErrorMessage from "./Components/ErrorMessage";
 import FoodItems from "./Components/FoodItems";
+import Container from "./Components/Container";
 function App()
 {
 
   const foodItems = ["apple", "papaya", "pomgranate", "grapes"];
   return (
     <>
-      <h1 className="food-heading">Healthy Foods</h1>
-      {
-        foodItems.length === 0 ? <ErrorMessage food={foodItems}></ErrorMessage> :
-          <FoodItems food={foodItems}></FoodItems>
-      }
+      <Container>
+        <h1 className="food-heading">Healthy Foods</h1>
+        {
+          foodItems.length === 0 ? <ErrorMessage food={foodItems}></ErrorMessage> :
+            <FoodItems food={foodItems}></FoodItems>
+        }
+      </Container>
+      {/* <Container>
+        <p>Above are healthy foods that are good for your health.</p>
+
+      </Container> */}
     </>
   );
 }
--- src/Components/Items.jsx
+++ src/Components/Items.jsx
@@ -3,7 +3,11 @@ import styles from "./Item.module.css";
 export default function Items({ food })
 {
     console.log(styles);
-    return (<> <li className={`${styles['pp-item']} list-group-item`}>
-        <span className={styles['pp-span']}>{food}</span>
-    </li></>);
+    return (<>
+        <li className={`${styles['pp-item']} list-group-item`}>
+            <span className={styles['pp-span']}>{food}</span>
+            <button onClick={() => console.log('buy button was clicked')} className={`${styles.button} btn btn-info`}> Buy </button>
+        </li>
+
+    </>);
 }
